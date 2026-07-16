3b3acb3ef6c7db11544ad5bb2f1e302f54e06ae8
s2vivasjimmy-lgtm <s2vivasjimmy@gmail.com>
2026-07-15 23:20:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ else:
             with cols[i % 4]:
                 st.markdown(f'<div class="compact-card"><div class="card-title">{iconos.get(col_name, "📊")} {col_name}</div><div class="card-value">{df[col_name].iloc[0]}</div></div>', unsafe_allow_html=True)
 
-        st.subheader("📍 UBICACIONES")
+        st.subheader("📍UBICACIONES EN TIEMPO REAL")
         st.components.v1.html("""
             <div id="map-container" style="position: relative; width: 100%; height: 500px; border: 1px solid #31333f; border-radius: 12px; overflow: hidden;">
                 <button onclick="toggleFS()" style="position: absolute; top: 10px; right: 10px; z-index: 1000; padding: 8px 12px; cursor: pointer; background: #ffffff; border: none; border-radius: 5px; font-weight: bold; box-shadow: 0 2px 5px rgba(0,0,0,0.3);">
