bd612b60eaa977a19312b7f6522f686381b1b421
s2vivasjimmy-lgtm <s2vivasjimmy@gmail.com>
2026-07-15 22:49:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ else:
             if user == "Admin" and pwd == "diges12..":
                 st.session_state.admin_logueado = True
                 st.rerun()
-
+                
    if os.path.exists("logo_institucional.jpg"):
     st.image("logo_institucional.jpg", width=300)
     st.markdown('<div class="marquee-container"><h2 class="marquee-text">AUTORIDAD ÚNICA DE SALUD MILITAR DEL ESTADO LA GUAIRA</h2></div>', unsafe_allow_html=True)
