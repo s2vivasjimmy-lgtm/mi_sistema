0d495d7de528217ea9e62e47dc5f3d29215d569c
s2vivasjimmy-lgtm <s2vivasjimmy@gmail.com>
2026-07-17 15:30:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ else:
             df_detalle = pd.read_csv(archivo_detalle, dtype=str).fillna("")
             st.dataframe(df_detalle, use_container_width=True, hide_index=True)
             
-            st.markdown("### Mapa de Ruta Epidemiológica")
+            st.markdown("### 📍Ubicación En Tiempo Real")
             st.components.v1.html("""
                 <div id="map-container-ruta" style="position: relative; width: 100%; height: 500px; border: 1px solid #31333f; border-radius: 12px; overflow: hidden;">
                     <button onclick="toggleFS('map-container-ruta')" style="position: absolute; top: 10px; right: 10px; z-index: 1000; padding: 8px 12px; cursor: pointer; background: #ffffff; border: none; border-radius: 5px; font-weight: bold; box-shadow: 0 2px 5px rgba(0,0,0,0.3);">
